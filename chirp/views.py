7931a4b25c5a77951b815e986c56c0a093c469e7
John Bryan <johnmbryan.25@gmail.com>
2016-04-15 23:04:15 -0700

    Fixing files

--- chirp/views.py
+++ chirp/views.py
@@ -3,7 +3,16 @@ from flask import render_template, request, redirect, url_for
 from yelpapi import YelpAPI
 
 import os
+import requests
+import json
+import urllib
 from chirp import app
+
+
+BASE_URL = "http://api.brewerydb.com/v2/"
+SEARCH_URL = "search?"
+API_key = "b23f19cf8c791e60adbf1fd26fa892d6"
+
 
 yelp_api = YelpAPI(os.environ['YELP_KEY'], os.environ['YELP_SECRET'],
                    os.environ['YELP_TOKEN'], os.environ['YELP_TOKEN_SECRET'])
@@ -20,16 +29,38 @@ def index():
 @app.route("/search")
 def search():
     try:
-        yelp_rs = yelp_api.search_query(location=request.args.get("location"))
+        
+        query = urllib.quote_plus(request.args.get("beer"))
+        qtype = "beer"
 
-        businesses = [
-            {"image_url": i['image_url'][:-6] + 'ls.jpg', "name": i["name"],
-             "description": i["snippet_text"], "rating": i["rating_img_url"],
-             "id": i["id"]}
-            for i in yelp_rs['businesses']]
+        response = requests.get(BASE_URL + SEARCH_URL + "key=" + API_key + "&type=" + qtype + "&q=" + query)
+        data = json.loads(response.content)
+        
+        
+        beers = []
+        for b in data['data']:
+            dict1 = {}
+            if 'name' in b.keys():
+                dict1['name'] = b['name']
+            if 'description' in b.keys():
+                dict1['description'] = b['description']
+            if 'labels' in b.keys():
+                dict1['image'] = b['labels']['large']
+            
+            if len(dict1) == 3:
+                beers.append(dict1)
+            
+            
+        
+#        return data['data'][0]['name']
+#        businesses = [
+#            {"image_url": i['image_url'][:-6] + 'ls.jpg', "name": i["name"],
+#             "description": i["snippet_text"], "rating": i["rating_img_url"],
+#             "id": i["id"]}
+#            for i in yelp_rs['businesses']]
     except (YelpAPI.YelpAPIError):
         return "Oops! Error!"
-    return render_template("index.html", businesses=businesses,
+    return render_template("index.html", businesses=beers,
                            search_page=True)
 
 
@@ -58,3 +89,12 @@ def favorites():
         businesses.append(restaurants[k])
     return render_template("index.html", businesses=businesses,
                            search_page=False)
+
+@app.route("/beer")
+def beer():
+    query = request.args.get("beer")
+    qtype = "beer"
+
+    response = requests.get(BASE_URL + SEARCH_URL + "key=" + API_key + "&type=" + qtype + "&q=" + query)
+    data = json.loads(response.content)
+    return data['data'][0]['name']
